cb568e7fc99e3c094a38de13f88749e55def893c
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-02-07 10:27:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Forecast from "./components/Forecats"
+import "./App.css"
+
+
+export default function App(){
+  return(
+    <div>
+      <Forecast />
+    </div>
+
+  )
+}
